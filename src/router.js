8c723f5defa6c92012a3c1d943e2de6577f6773d
Cyrill Martin <cyrill.martin@hey.com>
2021-12-22 20:17:29 +0100

    Change to firebase

--- src/router.js
+++ src/router.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
-import About from "./pages/About.vue";
 import Poll from "./pages/Poll.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: Home },
-    { path: "/about", component: About },
     {
       path: "/poll/:id",
       component: Poll,
