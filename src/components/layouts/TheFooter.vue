b2a403f934ca83e968e06a66ff8a6ae895f746ac
Cyrill Martin <cyrill.martin@hey.com>
2022-03-20 13:02:32 +0100

    Update year in footer

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-12">
-    &#169; 2021 Cyrill Martin - <a href="https://kmapper.com" target="_blank">kmapper GmbH</a> -
+    &#169; 2022 Cyrill Martin - <a href="https://kmapper.com" target="_blank">kmapper GmbH</a> -
     <a href="https://github.com/cyrill-martin/swisspolls" target="_blank"
       >GitHub</a
     >
